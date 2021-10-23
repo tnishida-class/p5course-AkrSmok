3a999f755ebf640cd763a37170b8bf331d27b6e2
AkrSmok <91934059+AkrSmok@users.noreply.github.com>
2021-10-24 04:57:59 +0900

    section2-q 1~3

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,22 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
+    let plus = i + j;
+     if(plus%2==0){
+       noFill();
+     }
+     else {
+       fill(196);
+     }
+     rect(size*i, size*j, size, size);
+     if (j<=2 && plus%2==1) {
+      fill(255, 0, 0);
+
+      }
+      if (j>=5 && plus%2==1) {
+        fill(0, 0, 0);
+      }
+      ellipse(size*i + size / 2, size*j + size / 2, size)
+      }
   }
 }
